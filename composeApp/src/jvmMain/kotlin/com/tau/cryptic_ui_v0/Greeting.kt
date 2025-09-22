e9a040dc97cf6e1ed0b12001f8f144d5f0e68adc
Jarom Anderson <jarbear82@gmail.com>
2025-09-22 06:16:28 -0800

    removed unnecessary files

--- composeApp/src/jvmMain/kotlin/com/tau/cryptic_ui_v0/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package com.tau.cryptic_ui_v0
-
-class Greeting {
-    private val platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
--- composeApp/src/jvmMain/kotlin/com/tau/cryptic_ui_v0/Platform.kt
+++
@@ -1,7 +0,0 @@
-package com.tau.cryptic_ui_v0
-
-class JVMPlatform {
-    val name: String = "Java ${System.getProperty("java.version")}"
-}
-
-fun getPlatform() = JVMPlatform()
